33922117e4f56debe6d1051596efb8d0e142eae1
tykipanda <alejandro10londono@gmail.com>
2026-05-09 23:18:34 -0500

    se agrego inter

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,3 +3,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Sidebar } from "@/components/Sidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
