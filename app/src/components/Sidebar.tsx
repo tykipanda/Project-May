9a63a2406bce0e30fa2b3fd8d88035f3c865f29a
tykipanda <alejandro10londono@gmail.com>
2026-05-10 22:58:13 -0500

    creacion variable  handNew en sidebar

--- app/src/components/Sidebar.tsx
+++ app/src/components/Sidebar.tsx
@@ -6,7 +6,13 @@ import { useChatStore } from "@/store/chatStore";
 import { PlusIcon, ChatBubbleIcon } from "@radix-ui/react-icons";
 
 export function Sidebar() {
-  const { conversations, createConversation } = useChatStore();
-  const pathname = usePathname();
+ const { conversations, createConversation } = useChatStore();
+ const pathname = usePathname();
+
+ const handleNew = () => {
+    const id = createConversation();
+    // router.push lo maneja el store
+  };
+
 
 
