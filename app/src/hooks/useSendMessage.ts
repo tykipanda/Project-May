70d26eae6f89a428801433fc2cf0c887fe0e4e6e
tykipanda <alejandro10londono@gmail.com>
2026-06-07 21:59:41 -0500

    creacion de variable conversation en useSendStore

--- app/src/hooks/useSendMessage.ts
+++ app/src/hooks/useSendMessage.ts
@@ -4,3 +4,6 @@ import { useChatStore } from "@/store/chatStore";
 export function useSendMessage(convId: string) {
   const { addMessage, appendToLastMessage, setStatus, setError } =
     useChatStore();
+      const conversation = useChatStore(s =>
+    s.conversations.find(c => c.id === convId)
+  );
