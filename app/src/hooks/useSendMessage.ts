5876a4c5c8680be67bcc6b95cbd146b8f39f455c
tykipanda <alejandro10londono@gmail.com>
2026-06-08 20:21:04 -0500

    creacion de body y messages

--- app/src/hooks/useSendMessage.ts
+++ app/src/hooks/useSendMessage.ts
@@ -24,3 +24,5 @@ export function useSendMessage(convId: string) {
       const response = await fetch("/api/chat", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          messages: [
